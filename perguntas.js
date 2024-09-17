2ffd6ffd8742ada58e7b0f67cbf976a25a7650b3
maria luciano <145155358+marialuciano08@users.noreply.github.com>
2024-09-17 16:22:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Livro Anne',
+    'Qual é o nome da casa onde Anne vive com os Cuthberts?',
+    'A casa onde Anne vive com os Cuthberts se chama Green Gables.'
+)
+
+criaCartao(
+    'Livro Trilogia do Verão',
+    'O que faz com que a vida da protagonista mudar durante o verão descrito no livro?',
+    'A vida de Belly muda durante o verão porque ela passa a enfrentar novas experiências e sentimentos ao crescer e lidar com a complexidade das relações amorosas e familiares.'
+)
+
+criaCartao(
+    'Livro a Rainha Vermelha',
+    'Qual é o nome da protagonista de A Rainha Vermelha e qual é a sua habilidade especial?',
+    'A protagonista do livro é Mare Barrow. Ela possui a habilidade de controlar a eletricidade, o que é raro e especial em sua sociedade.'
+)
+
+criaCartao(
+    'Livro A Ascensão do Dragão',
+    'Qual dinastia é retratada no livro, e qual é seu principal símbolo?',
+    'O livro retrata a dinastia Targaryen, cujo principal símbolo é o dragão.'
+)
